72c20ed345861e144a60ea782d09957825f1069f
vanessaramos0620 <159545078+vanessaramos0620@users.noreply.github.com>
2025-01-17 22:25:32 +0800

    Delete js directory

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-// Toggle menu with button
-u('#navBarButton').on('click', function(e) {
-    u('#navBarMenu').toggleClass('is-active')
-})
-
-// Hide menu on search
-u('#searchButton').on('click', function(e) {
-    u('#navBarMenu').removeClass('is-active')
-})
